ef766ed999e2873d4d1caddcbc2ba4cc6cd38c4b
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-06-13 14:55:11 -0700

    Fix throw

--- src/index.js
+++ src/index.js
@@ -429,7 +429,12 @@ class MockVM {
             throw new KoinosError("", koinos.chain.error_code.success)
           }
 
-          throw new KoinosError(UInt8ArrayToString(exit_args.res.error.message), exit_args.code)
+          if (exit_args.res && exit_args.res.error && exit_args.res.error.message)
+          {
+            throw new KoinosError(exit_args.res.error.message, exit_args.code)
+          }
+
+          throw new KoinosError("", exit_args.code)
         }
 
         case koinos.chain.system_call_id.get_arguments: {
